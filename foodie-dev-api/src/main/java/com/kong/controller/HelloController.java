f86ff38f5a709dd2994fa4a6801456aca6240b43
Haitang519 <jk2145@georgetown.edu>
2021-05-19 15:00:36 +0800

    Initial commit

---
+++ foodie-dev-api/src/main/java/com/kong/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.kong.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public Object hello() {
+        return "Hello world";
+    }
+}

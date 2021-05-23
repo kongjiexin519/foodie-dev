c5b7c08506012c4266b0cc6730f097a5977b3ecd
Haitang519 <jk2145@georgetown.edu>
2021-05-23 22:27:40 +0800

    开启Mybatis日志打印

--- foodie-dev-api/src/main/java/com/kong/controller/PassportController.java
+++ foodie-dev-api/src/main/java/com/kong/controller/PassportController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.swing.*;
 
 @RestController
 @RequestMapping("passport")
